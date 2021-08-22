279b4900912d42d5a580b26a1dd9f5b114f73dc8
NathanielKhallil <nathanielkhallil@gmail.com>
2021-08-22 14:21:14 -0600

    Deleted app.js, renamed apptest.js, set ind js src

--- javascript/app.js
+++ javascript/app.js
@@ -1,238 +1,101 @@
-const formOne = document.getElementById("year_one").elements;
-const formTwo = document.getElementById("year_two").elements;
-
-let buttonOne = document.getElementById('gpa_one');
-let buttonTwo = document.getElementById('gpa_two');
-let buttonThree = document.getElementById('gpa_three');
-let buttonFour = document.getElementById('gpa_four');
-
-let resetButtonOne = document.getElementById('reset_gpa1');
-let resetButtonTwo = document.getElementById('reset_gpa2');
-let resetButtonThree = document.getElementById('reset_gpa3');
-let resetButtonFour = document.getElementById('reset_gpa4');
-
-
-let checkUpdate = false;
 
 let combined_gpa = [];
 
 
 // Calculate GPA for year one in Alberta, Canada
 
-buttonOne.onclick = function () {
-    let year_one = [];
-    let first_year = [];
-    let gpa_one = [];
+function calculateGpaOne(arg) {
+    let element_list = arg.elements;
+    let number_list = [];
+    let letter_list = [];
+    let gpa_list = [];
 
-    for (let i = 0; i < formOne.length; i++)
-        if (formOne[i].value !== '')
-            year_one.push((formOne[i].value));
-    year_one = year_one.map(x => Number(x));
+    for (let i = 0; i < element_list.length; i++)
+        if (element_list[i].value !== '')
+            number_list.push((element_list[i].value));
+    number_list = number_list.map(x => Number(x));
 
-    for (let i = 0; i < year_one.length; i++) {
-        if (year_one[i] < 50)
-            first_year.push('F');
+    for (let i = 0; i < number_list.length; i++) {
+        if (number_list[i] < 50)
+            letter_list.push('F');
 
-        if (year_one[i] > 49 && year_one[i] < 55)
-            first_year.push('D');
+        if (number_list[i] > 49 && number_list[i] < 55)
+            letter_list.push('D');
 
-        if (year_one[i] > 54 && year_one[i] < 60)
-            first_year.push('D+');
+        if (number_list[i] > 54 && number_list[i] < 60)
+            letter_list.push('D+');
 
-        if (year_one[i] > 59 && year_one[i] < 64)
-            first_year.push('C-');
+        if (number_list[i] > 59 && number_list[i] < 64)
+            letter_list.push('C-');
 
-        if (year_one[i] > 63 && year_one[i] < 67)
-            first_year.push('C');
+        if (number_list[i] > 63 && number_list[i] < 67)
+            letter_list.push('C');
 
-        if (year_one[i] > 66 && year_one[i] < 70)
-            first_year.push('C+');
+        if (number_list[i] > 66 && number_list[i] < 70)
+            letter_list.push('C+');
 
-        if (year_one[i] > 69 && year_one[i] < 73)
-            first_year.push('B-');
+        if (number_list[i] > 69 && number_list[i] < 73)
+            letter_list.push('B-');
 
-        if (year_one[i] > 72 && year_one[i] < 76)
-            first_year.push('B');
+        if (number_list[i] > 72 && number_list[i] < 76)
+            letter_list.push('B');
 
-        if (year_one[i] > 75 && year_one[i] < 80)
-            first_year.push('B+');
+        if (number_list[i] > 75 && number_list[i] < 80)
+            letter_list.push('B+');
 
-        if (year_one[i] > 79 && year_one[i] < 85)
-            first_year.push('A-');
+        if (number_list[i] > 79 && number_list[i] < 85)
+            letter_list.push('A-');
 
-        if (year_one[i] > 84)
-            first_year.push('A');
+        if (number_list[i] > 84)
+            letter_list.push('A');
     }
 
-    for (let i = 0; i < first_year.length; i++) {
-        if (first_year[i] == 'f')
-            gpa_one.push(0);
-        if (first_year[i] == 'D')
-            gpa_one.push(1);
-        if (first_year[i] == 'D+')
-            gpa_one.push(1.3);
-        if (first_year[i] == 'C-')
-            gpa_one.push(1.7);
-        if (first_year[i] == 'C')
-            gpa_one.push(2);
-        if (first_year[i] == 'C+')
-            gpa_one.push(2.3);
-        if (first_year[i] == 'B-')
-            gpa_one.push(2.7);
-        if (first_year[i] == 'B')
-            gpa_one.push(3);
-        if (first_year[i] == 'B+')
-            gpa_one.push(3.3);
-        if (first_year[i] == 'A-')
-            gpa_one.push(3.7);
-        if (first_year[i] == 'A')
-            gpa_one.push(4);
+    for (let i = 0; i < letter_list.length; i++) {
+        if (letter_list[i] == 'f')
+            gpa_list.push(0);
+        if (letter_list[i] == 'D')
+            gpa_list.push(1);
+        if (letter_list[i] == 'D+')
+            gpa_list.push(1.3);
+        if (letter_list[i] == 'C-')
+            gpa_list.push(1.7);
+        if (letter_list[i] == 'C')
+            gpa_list.push(2);
+        if (letter_list[i] == 'C+')
+            gpa_list.push(2.3);
+        if (letter_list[i] == 'B-')
+            gpa_list.push(2.7);
+        if (letter_list[i] == 'B')
+            gpa_list.push(3);
+        if (letter_list[i] == 'B+')
+            gpa_list.push(3.3);
+        if (letter_list[i] == 'A-')
+            gpa_list.push(3.7);
+        if (letter_list[i] == 'A')
+            gpa_list.push(4);
     }
 
-
-    if (checkUpdate = true) {
-        let reset = document.getElementById('listOne');
-        reset.removeChild(reset.childNodes[48]);
-        combined_gpa = combined_gpa.splice(combined_gpa[0], 1);
-        console.log(combined_gpa);
-
-    }
-
-    let sum = gpa_one.reduce((a, b) => a + b);
-    let gpa = sum / first_year.length;
+    let sum = gpa_list.reduce((a, b) => a + b);
+    let gpa = sum / letter_list.length;
 
     gpa = Number((Math.abs(gpa) * 100).toPrecision(15));
     gpa = Math.round(gpa) / 100 * Math.sign(gpa)
 
-    combined_gpa.push(gpa);
-
-    const yearOneGpa = document.getElementById('listOne');
-
-    let gpaResult = document.createElement('p');
-    gpaResult.setAttribute("id", "result1");
-
-    gpaResult.textContent = 'Year 1 GPA:' + gpa;
-
-    yearOneGpa.appendChild(gpaResult);
-
-    checkUpdate = true;
-    console.log(combined_gpa);
+    const gpa_results = arg.querySelector('ul');
+    const gpa_input = gpa_results.lastElementChild;
+    gpa_input.value = gpa;  
 }
 
 // reset First Year GPA, delete entry from cummulative GPA
-resetButtonOne.onclick = function () {
-    let reset = document.getElementById("result1");
-    reset.textContent = '';
-    combined_gpa = combined_gpa.splice(combined_gpa[0], 1);
-    console.log(combined_gpa)
+function resetGpa(resultText) {
+    resultText.reset();
 }
 
 
-buttonTwo.onclick = function () {
-    let year_one = [];
-    let first_year = [];
-    let gpa_one = [];
-
-    for (let i = 0; i < formTwo.length; i++)
-        if (formTwo[i].value !== '')
-            year_one.push((formTwo[i].value));
-
-    year_one = year_one.map(x => Number(x));
-
-    for (let i = 0; i < year_one.length; i++) {
-        if (year_one[i] < 50)
-            first_year.push('F');
-
-        if (year_one[i] > 49 && year_one[i] < 55)
-            first_year.push('D');
-
-        if (year_one[i] > 54 && year_one[i] < 60)
-            first_year.push('D+');
-
-        if (year_one[i] > 59 && year_one[i] < 64)
-            first_year.push('C-');
-
-        if (year_one[i] > 63 && year_one[i] < 67)
-            first_year.push('C');
-
-        if (year_one[i] > 66 && year_one[i] < 70)
-            first_year.push('C+');
-
-        if (year_one[i] > 69 && year_one[i] < 73)
-            first_year.push('B-');
-
-        if (year_one[i] > 72 && year_one[i] < 76)
-            first_year.push('B');
-
-        if (year_one[i] > 75 && year_one[i] < 80)
-            first_year.push('B+');
-
-        if (year_one[i] > 79 && year_one[i] < 85)
-            first_year.push('A-');
-
-        if (year_one[i] > 84)
-            first_year.push('A');
-    }
-
-    for (let i = 0; i < first_year.length; i++) {
-        if (first_year[i] == 'f')
-            gpa_one.push(0);
-        if (first_year[i] == 'D')
-            gpa_one.push(1);
-        if (first_year[i] == 'D+')
-            gpa_one.push(1.3);
-        if (first_year[i] == 'C-')
-            gpa_one.push(1.7);
-        if (first_year[i] == 'C')
-            gpa_one.push(2);
-        if (first_year[i] == 'C+')
-            gpa_one.push(2.3);
-        if (first_year[i] == 'B-')
-            gpa_one.push(2.7);
-        if (first_year[i] == 'B')
-            gpa_one.push(3);
-        if (first_year[i] == 'B+')
-            gpa_one.push(3.3);
-        if (first_year[i] == 'A-')
-            gpa_one.push(3.7);
-        if (first_year[i] == 'A')
-            gpa_one.push(4);
-    }
 
 
-    if (checkUpdate = true) {
-        let reset = document.getElementById('listTwo');
-        reset.removeChild(reset.childNodes[48]);
-        combined_gpa = combined_gpa.splice(combined_gpa[0], 1);
-        console.log(combined_gpa);
+// cumulative GPA - under construction
 
-    }
-
-    let sum = gpa_one.reduce((a, b) => a + b);
-    let gpa = sum / first_year.length;
-
-    gpa = Number((Math.abs(gpa) * 100).toPrecision(15));
-    gpa = Math.round(gpa) / 100 * Math.sign(gpa)
-
-    combined_gpa.push(gpa);
-
-    const yearOneGpa = document.getElementById('listTwo');
-
-    let gpaResult = document.createElement('p');
-    gpaResult.setAttribute("id", "result2");
-
-    gpaResult.textContent = 'Year 2 GPA:' + gpa;
-
-    yearOneGpa.appendChild(gpaResult);
-
-    checkUpdate = true;
-    console.log(combined_gpa);
-}
-
-// reset First Year GPA, delete entry from cummulative GPA
-resetButtonTwo.onclick = function () {
-    let reset = document.getElementById("result2");
-    reset.textContent = '';
-    combined_gpa = combined_gpa.splice(combined_gpa[0], 1);
-    console.log(combined_gpa)
-}
+// combined_gpa.push(gpa);
+// combined_gpa = combined_gpa.splice(combined_gpa[0], 1);
+// console.log(combined_gpa)
