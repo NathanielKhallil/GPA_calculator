84f185b233794654edbc4ef98492c03398b30bde
NathanielKhallil <78049880+NathanielKhallil@users.noreply.github.com>
2021-08-26 10:19:54 -0600

    Reduced funciton code, removed letterList

--- javascript/app.js
+++ javascript/app.js
@@ -23,7 +23,6 @@ window.onload = function() {
 function calculateGpa(arg) {
     let elementList = arg.elements;
     let numberList = [];
-    let letterList = [];
     let gpaList = [];
     
     const testForRerun = arg.querySelector('ul').lastElementChild.lastElementChild;
@@ -39,66 +38,43 @@ function calculateGpa(arg) {
     
     for (let i = 0; i < numberList.length; i++) {
         if (numberList[i] < 50)
-            letterList.push('F');
+            gpaList.push(0);
 
         if (numberList[i] > 49 && numberList[i] < 55)
-            letterList.push('D');
+            gpaList.push(1);
 
         if (numberList[i] > 54 && numberList[i] < 60)
-            letterList.push('D+');
+            gpaList.push(1.3);
 
         if (numberList[i] > 59 && numberList[i] < 64)
-            letterList.push('C-');
+            gpaList.push(1.7);
 
         if (numberList[i] > 63 && numberList[i] < 67)
-            letterList.push('C');
+            gpaList.push(2);
 
         if (numberList[i] > 66 && numberList[i] < 70)
-            letterList.push('C+');
+            gpaList.push(2.3);;
 
         if (numberList[i] > 69 && numberList[i] < 73)
-            letterList.push('B-');
+            gpaList.push(2.7);
 
         if (numberList[i] > 72 && numberList[i] < 76)
-            letterList.push('B');
+            gpaList.push(3);
 
         if (numberList[i] > 75 && numberList[i] < 80)
-            letterList.push('B+');
+            gpaList.push(3.3);
 
         if (numberList[i] > 79 && numberList[i] < 85)
-            letterList.push('A-');
+            gpaList.push(3.7);
 
         if (numberList[i] > 84)
-            letterList.push('A');
-    }
-        
-    for (let i = 0; i < letterList.length; i++) {
-        if (letterList[i] == 'f')
-            gpaList.push(0);
-        if (letterList[i] == 'D')
-            gpaList.push(1);
-        if (letterList[i] == 'D+')
-            gpaList.push(1.3);
-        if (letterList[i] == 'C-')
-            gpaList.push(1.7);
-        if (letterList[i] == 'C')
-            gpaList.push(2);
-        if (letterList[i] == 'C+')
-            gpaList.push(2.3);
-        if (letterList[i] == 'B-')
-            gpaList.push(2.7);
-        if (letterList[i] == 'B')
-            gpaList.push(3);
-        if (letterList[i] == 'B+')
-            gpaList.push(3.3);
-        if (letterList[i] == 'A-')
-            gpaList.push(3.7);
-        if (letterList[i] == 'A')
             gpaList.push(4);
     }
+        console.log(gpaList);
+
     
     let sum = gpaList.reduce((a, b) => a + b);
-    let gpa = sum / letterList.length;
+    let gpa = sum / numberList.length;
 
     gpa = Number((Math.abs(gpa) * 100).toPrecision(15));
     gpa = Math.round(gpa) / 100 * Math.sign(gpa)
@@ -108,7 +84,7 @@ function calculateGpa(arg) {
     gpaInput.value = gpa;  
 }
 
-// reset First Year GPA, delete entry from cummulative GPA
+// reset First Year GPA, delete entry from cumulative GPA
 function resetGpa(resultText) {
     resultText.reset();
 }
@@ -140,3 +116,31 @@ totalButton.onclick = function() {
 
 
 }
+
+
+
+// code for potential letter grade version
+    // for (let i = 0; i < letterList.length; i++) {
+    //     if (letterList[i] == 'f')
+    //         gpaList.push('0');//
+    //     if (letterList[i] == 'D')
+    //         gpaList.push(1); //
+    //     if (letterList[i] == 'D+')
+    //         gpaList.push(1.3);//
+    //     if (letterList[i] == 'C-')
+    //         gpaList.push(1.7);//
+    //     if (letterList[i] == 'C')
+    //         gpaList.push(2);//
+    //     if (letterList[i] == 'C+')
+    //         gpaList.push(2.3);//
+    //     if (letterList[i] == 'B-')
+    //         gpaList.push(2.7);//
+    //     if (letterList[i] == 'B')
+    //         gpaList.push(3);//
+    //     if (letterList[i] == 'B+')
+    //         gpaList.push(3.3);//
+    //     if (letterList[i] == 'A-')
+    //         gpaList.push(3.7);//
+    //     if (letterList[i] == 'A')
+    //         gpaList.push(4);
+    // }
